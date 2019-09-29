06bcfbd729cf62aca73b89f73ade9c98b2f8b35e
Sepehr Laal <sepehr.laal@gmail.com>
2019-09-28 20:42:58 -0700

    proof of concept checked in
    this is actually not going to be suitable for peer engine as API Gateway has a 2-hours limit on connections. Good practice but not for production

--- index.js
+++ index.js
@@ -1,3 +1,113 @@
-class MonologueClient {}
+'use strict';
 
+const _ = require('lodash');
+const rc = require('rc');
+const once = require('./once');
+const debug = require('debug')('monologue');
+const assert = require('assert');
+const uniqid = require('uniqid');
+const Promise = require('bluebird');
+const WebSocket = require('ws');
+const { EventEmitter2 } = require('eventemitter2');
+
+class MonologueClient extends EventEmitter2 {
+  constructor(opts) {
+    super({ wildcard: true, maxListeners: getConfig().config.listeners });
+    this._id = uniqid('client');
+    this._opts = _.defaultsDeep(opts, getConfig().config);
+    this._callbacks = [];
+    this._connected = false;
+    this._connectionId = '';
+    debug('a new client is created. id=%s opts=%o', this._id, this._opts);
+
+    this.connect = once(this._doConnect.bind(this), { reentrant: true });
+    this.close = once(this._doClose.bind(this), { reentrant: true });
+  }
+
+  get connectionId() {
+    return this._connectionId;
+  }
+
+  async _doConnect() {
+    assert.ok(!this._connected);
+    debug('connecting to websocket endpoint: %s', this._opts.endpoint);
+    this._ws = new WebSocket(this._opts.endpoint);
+    this._ws.once('close', this.close);
+    this._ws.once('error', this.close);
+    await new Promise(resolve => {
+      this._ws.once('open', () => {
+        debug('connection is open for %s - sending the ID message', this._id);
+        const token = uniqid(this._id);
+        this._ws.send(JSON.stringify({ action: 'loopback', payload: { token } }));
+        this._ws.once('message', data => {
+          debug('ID message received for %s:%s', this._id, data);
+          const { to, from, payload } = JSON.parse(data);
+          assert.ok(_.get(payload, 'token') === token);
+          assert.ok(to === from);
+          this._connectionId = to;
+          this._connected = true;
+          this._ws.on('message', this._messageLoop.bind(this));
+          this.emit('connect');
+          resolve();
+        });
+      });
+    }).timeout(+this._opts.timeout);
+  }
+
+  async _messageLoop(what) {
+    debug('message received for %s:%s', this._id, what);
+    const { to, from, payload } = JSON.parse(what);
+    assert.ok(to === this._connectionId);
+
+    const type = _.get(payload, 'type', '');
+    const token = _.get(payload, 'token', 'invalid');
+
+    if (type === 'REQ') {
+      const name = _.get(payload, 'name', '');
+      const args = _.get(payload, 'args', []);
+      const fn = _.first(this.listeners(name));
+      assert.ok(_.isFunction(fn));
+      const ret = await fn.apply(null, args);
+      this._ws.send(JSON.stringify({ to: from, action: 'sendmessage', payload: { token, ret, type: 'ACK' } }));
+    } else if (type === 'ACK') {
+      assert.ok(this._callbacks[token]);
+      const ret = _.get(payload, 'ret');
+      this._callbacks[token](ret);
+      delete this._callbacks[token];
+    } else {
+      debug('invalid message type: %s', type);
+      assert.ok(false);
+    }
+  }
+
+  async call(to, name, ...args) {
+    assert.ok(this._connected);
+    const token = uniqid(this._id);
+    this._ws.send(JSON.stringify({ to, action: 'sendmessage', payload: { token, name, args, type: 'REQ' } }));
+    return await new Promise(resolve => {
+      this._callbacks[token] = resolve;
+    })
+      .timeout(this._opts.timeout)
+      .catch(err => {
+        debug('call with token %s expired without a response: %o', token, err);
+        delete this._callbacks[token];
+      });
+  }
+
+  async _doClose() {
+    assert.ok(this._connected);
+    this._ws.close();
+    this._connected = false;
+    this._connectionId = '';
+  }
+}
+
+const DEFAULT_CONFIG = {
+  config: { endpoint: '', timeout: 5000, listeners: 100 },
+};
+const USER_CONFIG = rc('monologue', DEFAULT_CONFIG);
+const CONFIG = _.assign({}, DEFAULT_CONFIG, USER_CONFIG);
+const getConfig = () => CONFIG;
+
+Promise.config({ cancellation: true });
 module.exports = MonologueClient;
--- test/client.test.js
+++ test/client.test.js
@@ -1,12 +1,75 @@
-const chai = require("chai");
-const chaiAP = require("chai-as-promised");
+'use strict';
 
-const Monologue = require("../index");
+const once = require('../once');
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiAP = require('chai-as-promised');
+const Promise = require('bluebird');
+
+const Monologue = require('../index');
 
 chai.use(chaiAP);
 
-describe("Monologue client tests", () => {
-  it("should always pass", () => {
+describe('Monologue client tests', () => {
+  it('should always pass', () => {
     chai.assert.isTrue(true);
+  });
+
+  it('should be able to connect and disconnect', async () => {
+    const callback = sinon.fake();
+
+    const mc1 = new Monologue();
+    await mc1.connect();
+
+    const mc2 = new Monologue();
+    await mc2.connect();
+
+    mc1.on('sample-method', async arg1 => {
+      callback(arg1);
+      return 'data';
+    });
+
+    const ret = await mc2.call(mc1.connectionId, 'sample-method', 'arg1');
+    chai.assert.equal(ret, 'data');
+    chai.assert.ok(callback.calledOnce);
+    chai.assert.ok(callback.calledWith('arg1'));
+
+    await Promise.all([mc1.close(), mc2.close()]);
+  });
+
+  it('should call an async function only once', async () => {
+    const callback = sinon.fake();
+    const returns = { test: 'data' };
+    const error = new Error('test');
+
+    async function incrementor() {
+      await Promise.delay(50);
+      callback();
+      // to check returns
+      if (callback.calledTwice) return returns;
+      if (callback.calledThrice) throw error;
+    }
+
+    const incrementOnce = once(incrementor);
+    // firing two of these at the same time should only call one of them
+    await chai.assert.isFulfilled(Promise.all([incrementOnce(), incrementOnce()]));
+    chai.assert.isTrue(callback.calledOnce);
+    // calling it again should not work since it's not marked reentrant
+    await chai.assert.isFulfilled(incrementOnce());
+    chai.assert.isTrue(callback.calledOnce);
+    callback.resetHistory();
+
+    const incrementOnceRE = once(incrementor, { reentrant: true });
+    // firing two of these at the same time should only call one of them
+    await chai.assert.isFulfilled(Promise.all([incrementOnceRE(), incrementOnceRE()]));
+    chai.assert.isTrue(callback.calledOnce);
+    // calling it again should work since it's marked reentrant
+    const data = await chai.assert.isFulfilled(incrementOnceRE());
+    chai.assert.isTrue(callback.calledTwice);
+    chai.assert.deepEqual(data, returns);
+    // test error propagation
+    await chai.assert.isRejected(incrementOnceRE());
+    chai.assert.isTrue(callback.calledThrice);
+    callback.resetHistory();
   });
 });
